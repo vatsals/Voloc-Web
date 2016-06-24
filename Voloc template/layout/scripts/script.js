66241d1c0c87752dbf1a180022cf0fc7cead2f41
vatsals <vatsalshrivastav26@gmail.com>
2016-06-25 03:16:24 +0530

    Navbar, height and iframes added

---
+++ Voloc template/layout/scripts/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+            
+			$(".navbar_div").load("navbar.html");
+			
+			$('.category').hover(
+
+                function() {
+                    $(this).find('h2').attr('class', 'changedclass');
+                    $(this).find('img').attr('class', 'addslide');
+                },
+
+                function() {
+                    $(this).find('h2').attr('class', 'originalclass');
+                    $(this).find('img').attr('class', 'removeslide');
+                }
+                );
+
+        });
+        function showTeam(){
+            $('.team-frame').css('marginLeft','250px');
+            $('.team-frame').fadeIn(1500);
+            $('#team').addClass('active');
+        }
+		
+        function openNav(paramsrc) {
+			
+			$(".changeframes").attr('src',paramsrc);
+            document.getElementById("mySidenav").style.width = "250px";
+            $("#top").attr('class', 'toporiginal');
+			$(".changeframes").fadeIn(1500);
+			$('.changeframes').css('marginLeft','250px');
+        }
+
+        /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+            $("#top").attr('class', 'topchanged');
+            $('iframe').animate({
+                marginLeft : 0
+            },1000);
+            $('iframe').fadeOut(1500);
+        }
